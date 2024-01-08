a07eda3b64384b37a4b5d965c1b4ac5273bd8c05
MANISH KUMAR PRAJAPATI <71719780+ManishkPrajapati@users.noreply.github.com>
2024-01-08 11:31:51 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import MarkSheet from './components/MrakSheet'
+import Student from './components/Student'
+
+function App() {
+
+  return (
+    <>
+    <MarkSheet/>
+    <Student/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/MrakSheet.jsx
@@ -0,0 +1,7 @@
+import styles from "./MarkSheet.module.css";
+
+const MarkSheet = () => {
+  return <p className={styles.mark}>Student MarkSheet</p>;
+};
+
+export default MarkSheet;
